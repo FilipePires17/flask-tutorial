5901d56c9c8a5fdbfa3fc94622682bfcdaddf679
Filipe Pires <filipepiresec@gmail.com>
2025-09-18 17:17:50 -0400

    mais quatro

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_object(Config)
 
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
 db.init_app(app)
 app.register_blueprint(user_bp, url_prefix="/users")
 
 with app.app_context():
     db.create_all()
-
-CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
