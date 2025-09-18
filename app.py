10370a6860f11838e341c565cae4ee5f302dee5d
Filipe Pires <filipepiresec@gmail.com>
2025-09-18 15:51:49 -0400

    mais tres

--- app.py
+++ app.py
@@ -5,28 +5,16 @@ from blueprints.user.routes import user_bp
 from config import Config
 from flask_cors import CORS
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    app.config.from_object(Config)
+db.init_app(app)
+app.register_blueprint(user_bp, url_prefix="/users")
 
+with app.app_context():
+    db.create_all()
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    
-    db.init_app(app)
-
-    app.register_blueprint(user_bp, url_prefix="/users")
-
-    with app.app_context():
-        db.create_all()
-
-    CORS(app)
-
-    return app
+CORS(app)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
